faa150498b32ed885c4253635b23ff36bb465f38
Jerrod <chaddjed@gmail.com>
2023-10-25 10:37:16 -0700

    add 3 by 3 boxes

--- src/App.js
+++ src/App.js
@@ -5,10 +5,28 @@ import './App.css'
 const App = () => {
   const [squares, setSquares] = useState(Array(9).fill(null))
 
+  const handleClick = (index) => {
+    const newSquares = [...squares]
+    newSquares[index] = 'X'
+    setSquares(newSquares)
+    
+
+  }
+
   return (
     <>
       <h1>Tic Tac Toe</h1>
-      <Square />
+      <div className="gameboard">
+        {squares.map((value, index) => {
+          return(
+          <Square 
+          key={index}
+          value={value} 
+          onClick={() => handleClick(index)}
+          />
+        )
+        })}
+      </div>
     </>
   )
 }
